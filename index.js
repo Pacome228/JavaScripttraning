1b17e241640b71e96df1516c60f1e9e98c13680a
Pacome <pacome@Pacomes-MBP.lan>
2021-03-06 16:25:27 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//like i am never tired i learn all langage
+//console.log('Voici mon javascript begining');
+//let firstName = 'Dandji';
+//let lastName = 'Ayawo';
+//console.log(firstName,lastName);
+let person = {
+    name:'Pacome',
+    prenom:'Ayawo',
+    age:30
+    };
+
+console.log(person.age);
+
+let captile = ['lome', 'benin', 'accra'];
+captile[3] = 'cinkasse';
+console.log(captile);
+console.log(captile.length);
